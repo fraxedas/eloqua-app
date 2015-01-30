46a9efd40f1778d88ceeca63c7020dd989e6e6b9
Oscar Fraxedas <fraxedas@gmail.com>
2015-01-30 06:52:48 -0500

    now we use express

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var http = require("http");
-//var express = require("express");
+var express = require("express");
+var app = express();
 
-var server = http.createServer(function(req, resp){
-	console.log(req.url);
-	resp.write(req.url)
-	resp.end();
-})
+app.get("/", function(req, res){
+	res.send("Hello World");
+});
+
+app.get("/user", function(req, res){
+	//res.set("Content-Type", "application/json")
+	res.send({name : "Oscar Fraxedas", isValid: true});
+});
+
+var server = http.createServer(app);
 
 server.listen(3000);
