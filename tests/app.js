b6ea9891a20c28df59bb2e1b5ef9f863306beb6c
Oscar Fraxedas <fraxedas@gmail.com>
2015-02-24 16:26:08 -0500

    Adding mocha tests

---
+++ tests/app.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+
+describe('The Feature', function(){
+    describe('The test', function(){
+        it('should be equals', function(){
+            assert.equal(-1, -1);
+        })
+    })
+});
