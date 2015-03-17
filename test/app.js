8f77e7f34b6d2da3ffaa0c4bd3439bb0a7303ecc
Oscar Fraxedas <fraxedas@gmail.com>
2015-03-17 07:05:21 -0400

    fixing tests for mocha

--- test/app.js
+++ test/app.js
@@ -3,7 +3,7 @@ var assert = require("assert");
 describe('The Feature', function(){
     describe('The scenario', function(){
         it('should ...', function(){
-            //Todo: Add your assertions
+            assert.ok(true);
         })
     })
 });
