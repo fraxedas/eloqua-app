b16072d20c47a0800dc45fcc9435b77239436e5c
Oscar Fraxedas <oscar.fraxedas.tormo@oracle.com>
2015-07-24 07:30:51 -0400

    Add a few endpoints to manage app info

--- controllers/oauthController.js
+++ controllers/oauthController.js
@@ -3,16 +3,14 @@
     var persist = require('node-persist');
     persist.initSync();
     var eloquaOauth = require("../lib/eloqua-oauth");
-    var appId = 'id';
-    var appSecret = 'oscar';
     
     oauthController.init = function(oauth){
 		
 		oauth.all("/oauth/:appId/:installId", function(req, res){
             eloquaOauth.authorize({
-                client_id: appId,
+                client_id: req.params.appId,
                 redirect_uri: req.protocol + "://" + req.get('host') + '/callback/{appId}/{installId}',
-                state: req.params.appId + ':' + req.params.installId
+                state: req.params.installId
             }, function (uri, status) {
                 res.set('Location', uri).status(status).send();
             });
@@ -20,6 +18,8 @@
 
         
         oauth.all("/callback/:appId/:installId", function(req, res){
+            var appId = req.params.appId;
+            var client_secret = persist.getItem(req.params.appId);
             var code = req.query.code;
             var state = req.query.state;
             console.log(state);
@@ -27,7 +27,7 @@
             var authenticate = {
                 code: code,
                 redirect_uri: req.protocol + "://" + req.get('host') + '/callback/{appId}/{installId}',
-                client_secret: appSecret
+                client_secret: client_secret
             };
             eloquaOauth.grant(authenticate, function (error, body) {
                 if (error) {
@@ -38,9 +38,35 @@
                         error: error
                     });
                 }else{
-                    persist.setItem('oauth',body);
-                    res.redirect("/apps");
+                    persist.setItem(appId + '_oauth',body);
+                    res.redirect("/oauth/" + appId);
                 }
+            });
+        });
+        
+        oauth.post("/secret/:appId/:appSecret", function(req, res){
+            var appId = req.params.appId;
+            var appSecret = req.params.appSecret;
+            persist.setItem(appId,appSecret);
+                    
+            res.redirect("/secret/" + appId);
+        });
+        
+        oauth.get("/secret/:appId", function(req, res){
+            var appId = req.params.appId;
+                    
+            res.send({
+                appId: appId,
+                appSecret: persist.getItem(appId)
+            });
+        });
+        
+        oauth.get("/oauth/:appId", function(req, res){
+            var appId = req.params.appId;
+                    
+            res.send({
+                appId: appId,
+                oauth: persist.getItem(appId + '_oauth')
             });
         });
   
